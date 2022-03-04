9223740d0405c32ca6222b2b3d1e8586f7b2d892
Anton Patsev <patsev.anton@gmail.com>
2022-03-04 11:14:31 +0600

    Add Yandex Cloud compute default zone to 02-simple-run-instances

--- 02-simple-run-instances/provider.tf
+++ 02-simple-run-instances/provider.tf
@@ -11,4 +11,5 @@ provider "yandex" {
   token     = var.yc_token
   cloud_id  = var.yc_cloud_id
   folder_id = var.yc_folder_id
+  zone      = var.yc_zone
 }
--- 02-simple-run-instances/variable.tf
+++ 02-simple-run-instances/variable.tf
@@ -12,3 +12,9 @@ variable "yc_folder_id" {
   type = string
   description = "Yandex Cloud folder id"
 }
+
+variable "yc_zone" {
+  type = string
+  description = "Yandex Cloud compute default zone"
+  default = "ru-central1-c"
+}
