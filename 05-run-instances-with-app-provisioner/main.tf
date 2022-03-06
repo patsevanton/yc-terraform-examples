9a7adad8bd2e8ef5ded5f6f72f952d67a696249c
Anton Patsev <patsev.anton@gmail.com>
2022-03-06 18:15:48 +0600

    05-run-instances-with-app-provisioner

---
+++ 05-run-instances-with-app-provisioner/main.tf
@@ -0,0 +1,61 @@
+data "yandex_compute_image" "ubuntu-20-04" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+
+  name        = "linux-vm"
+  platform_id = "standard-v3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-20-04.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo DEBIAN_FRONTEND=noninteractive apt-get install mc jq -y",
+      "sudo touch /test.txt"
+    ]
+    connection {
+      type = "ssh"
+      user = var.user
+      private_key = file(var.private_key_path)
+      host = self.network_interface[0].nat_ip_address
+    }
+  }
+
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name       = "subnet1"
+  zone       = "ru-central1-c"
+  network_id = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+# Output values
+output "public-ip-address-for-vm-1" {
+  description = "Public IP address for vm-1"
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
---
+++ 05-run-instances-with-app-provisioner/variable.tf
@@ -0,0 +1,30 @@
+variable "yc_token" {
+  type = string
+  description = "Yandex Cloud API key"
+}
+
+variable "yc_cloud_id" {
+  type = string
+  description = "Yandex Cloud id"
+}
+
+variable "yc_folder_id" {
+  type = string
+  description = "Yandex Cloud folder id"
+}
+
+variable "yc_zone" {
+  type = string
+  description = "Yandex Cloud compute default zone"
+  default = "ru-central1-c"
+}
+
+variable "public_key_path" {
+  description = "Path to ssh public key, which would be used to access workers"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key_path" {
+  description = "Path to ssh private key, which would be used to access workers"
+  default     = "~/.ssh/id_rsa"
+}
