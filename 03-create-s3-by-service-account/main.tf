d5d6ce09b7da255cb5536f35eaa13bf5983d9fa7
Anton Patsev <patsev.anton@gmail.com>
2022-03-06 10:03:30 +0600

    Add resource "yandex_storage_bucket" "test"

--- 03-create-s3-by-service-account/main.tf
+++ 03-create-s3-by-service-account/main.tf
@@ -16,3 +16,10 @@ resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = yandex_iam_service_account.sa.id
   description        = "static access key for object storage"
 }
+
+// Use keys to create bucket
+resource "yandex_storage_bucket" "test" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "test-test-foobar"
+}
