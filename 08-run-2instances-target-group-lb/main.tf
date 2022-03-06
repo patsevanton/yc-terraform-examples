7596195bd1c415efd58f6054344e73bd74787539
Anton Patsev <patsev.anton@gmail.com>
2022-03-06 18:34:40 +0600

    08-run-2instances-target-group-lb

---
+++ 08-run-2instances-target-group-lb/main.tf
@@ -0,0 +1,84 @@
+resource "yandex_compute_instance" "vm-1" {
+
+  name        = "linux-vm"
+  platform_id = "standard-v3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd83n3uou8m03iq9gavu"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    user-data = file("cloud-init.yaml")
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name       = "subnet1"
+  zone       = "ru-central1-c"
+  network_id = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+# Output values
+output "public-ip-address-for-vm-1" {
+  description = "Public IP address for vm-1"
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+
+resource "yandex_lb_target_group" "loadbalancer" {
+  name      = "lb-group"
+  folder_id = var.yc_folder_id
+
+  target {
+    address = yandex_compute_instance.vm-1.network_interface.0.ip_address
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "lb" {
+  name = "loadbalancer"
+  type = "external"
+
+  listener {
+    name        = "listener"
+    port        = 80
+    target_port = 80
+
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.loadbalancer.id
+
+    healthcheck {
+      name = "tcp"
+      tcp_options {
+        port = 80
+      }
+    }
+  }
+}
+
+output "loadbalancer_ip_address" {
+  value = yandex_lb_network_load_balancer.lb.listener.*.external_address_spec[0].*.address
+}
---
+++ 08-run-2instances-target-group-lb/readme.md
@@ -0,0 +1,57 @@
+### Get FOLDER_ID
+```
+FOLDER_ID=$(yc config get folder-id)
+```
+
+### Create service-account
+```
+yc iam service-account create --name computeadmin
+```
+
+### Get id of service-account
+```
+SA_ID=$(yc iam service-account get --name computeadmin --format json | jq .id -r)
+```
+
+### Assign a role to the computeadmin service account using its ID:
+```
+yc resource-manager folder add-access-binding --id $FOLDER_ID --role compute.admin --subject serviceAccount:$SA_ID
+```
+### Get subnet list
+```
+yc vpc subnet list
+```
+
+### Show availability zones
+```
+yc compute zone list
+```
+
+### Create virtual machine (instances)
+```
+yc compute instance create \
+    --service-account-name computeadmin \
+    --name first-instance \
+    --zone ru-central1-b \
+    --network-interface subnet-name=apatsev-ru-central1-b,nat-ip-version=ipv4 \
+    --create-boot-disk image-folder-id=standard-images,image-family=centos-7 \
+    --ssh-key ~/.ssh/id_rsa.pub
+```
+
+### List virtual machine (instances)
+```
+yc compute instance list
+```
+
+### Delete the specified virtual machine (instances)
+```
+yc compute instance delete first-instance
+```
+
+Links:
+ - https://github.com/yandex-cloud/terraform-provider-yandex/issues/130
+ - https://github.com/sport24ru/terraform-yandex-managed-kubernetes/blob/master/main.tf
+ - https://vc.ru/dev/318266-kak-sekonomit-vremya-dengi-i-nervy-na-prostyh-zadachah
+ - https://github.com/yandex-cloud/terraform-provider-yandex/issues/121
+ - https://habr.com/ru/company/rebrainme/blog/538410/
+ 
