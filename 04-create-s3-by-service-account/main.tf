59146df5d1a5ce429a000733df2e6b173cfe30a9
Anton Patsev <patsev.anton@gmail.com>
2022-03-06 11:40:32 +0600

    Change test-bucket to test-bucket-anton-patsev

--- 04-create-s3-by-service-account/main.tf
+++ 04-create-s3-by-service-account/main.tf
@@ -18,22 +18,22 @@ resource "yandex_iam_service_account_static_access_key" "sa-storage-admin-static
 }
 
 ## Use keys to create bucket
-resource "yandex_storage_bucket" "test-bucket" {
+resource "yandex_storage_bucket" "test-bucket-anton-patsev" {
   access_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.secret_key
-  bucket     = "test-bucket"
+  bucket     = "test-bucket-anton-patsev"
 }
 
 ## Output values
 output "access_key_sa_storage_admin_for_test_bucket" {
-  description = "access_key sa-storage-admin for test-bucket"
-  value = yandex_storage_bucket.test-bucket.access_key
+  description = "access_key sa-storage-admin for test-bucket-anton-patsev"
+  value = yandex_storage_bucket.test-bucket-anton-patsev.access_key
   sensitive = true
 }
 
 output "secret_key_sa_storage_admin_for_test_bucket" {
-  description = "secret_key sa-storage-admin for test-bucket"
-  value = yandex_storage_bucket.test-bucket.secret_key
+  description = "secret_key sa-storage-admin for test-bucket-anton-patsev"
+  value = yandex_storage_bucket.test-bucket-anton-patsev.secret_key
   sensitive = true
 }
 
