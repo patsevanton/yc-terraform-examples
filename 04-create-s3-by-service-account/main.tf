65fbc5c16fdc290803f0410120d21f8b5d8b3c2a
Anton Patsev <patsev.anton@gmail.com>
2022-03-06 11:37:03 +0600

    Create output access_key_sa_storage_admin_for_test_bucket

--- 04-create-s3-by-service-account/main.tf
+++ 04-create-s3-by-service-account/main.tf
@@ -18,20 +18,22 @@ resource "yandex_iam_service_account_static_access_key" "sa-storage-admin-static
 }
 
 ## Use keys to create bucket
-resource "yandex_storage_bucket" "test" {
+resource "yandex_storage_bucket" "test-bucket" {
   access_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.access_key
   secret_key = yandex_iam_service_account_static_access_key.sa-storage-admin-static-key.secret_key
-  bucket     = "test-test-foobar"
+  bucket     = "test-bucket"
 }
 
 ## Output values
-output "access_key" {
-  description = "access_key sa-storage-admin"
-  value = yandex_storage_bucket.access_key
+output "access_key_sa_storage_admin_for_test_bucket" {
+  description = "access_key sa-storage-admin for test-bucket"
+  value = yandex_storage_bucket.test-bucket.access_key
+  sensitive = true
 }
 
-output "secret_key" {
-  description = "secret_key sa-storage-admin"
-  value = yandex_storage_bucket.secret_key
+output "secret_key_sa_storage_admin_for_test_bucket" {
+  description = "secret_key sa-storage-admin for test-bucket"
+  value = yandex_storage_bucket.test-bucket.secret_key
+  sensitive = true
 }
 
