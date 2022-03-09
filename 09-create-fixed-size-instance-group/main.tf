6613bc5e95e5bbb877b5fa3519b080c9bedd9161
Anton Patsev <patsev.anton@gmail.com>
2022-03-09 12:26:35 +0600

    resource yandex_resourcemanager_folder_iam_member in -9

--- 09-create-fixed-size-instance-group/main.tf
+++ 09-create-fixed-size-instance-group/main.tf
@@ -2,10 +2,24 @@ data "yandex_compute_image" "ubuntu-20-04" {
   family = "ubuntu-2004-lts"
 }
 
+## Create SA sa-compute-admin
+resource "yandex_iam_service_account" "sa-compute-admin" {
+  folder_id = var.yc_folder_id
+  name      = "sa-compute-admin"
+}
+
+## Grant permissions sa-compute-admin
+resource "yandex_resourcemanager_folder_iam_member" "sa-compute-admin-permissions" {
+  folder_id = var.yc_folder_id
+  role      = "compute.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-compute-admin.id}"
+}
+
 resource "yandex_compute_instance" "vm-1" {
 
   name        = "linux-vm"
   platform_id = "standard-v3"
+  service_account_id  = yandex_iam_service_account.sa-compute-admin.id
 
   resources {
     cores  = 2
