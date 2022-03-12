13d43be4253dcea6de280a5dcf63af400db402b3
Anton Patsev <patsev.anton@gmail.com>
2022-03-12 13:45:19 +0600

    Print execution time of a shell command

--- 14-provisioner-active-directory/active_directory_destroy.sh
+++ 14-provisioner-active-directory/active_directory_destroy.sh
@@ -2,4 +2,7 @@
 
 set -e
 
+start_time=`date +%s`
 TF_IN_AUTOMATION=1 terraform destroy -auto-approve
+end_time=`date +%s`
+echo execution time was `expr $end_time - $start_time` s.
--- 14-provisioner-active-directory/active_directory_install.sh
+++ 14-provisioner-active-directory/active_directory_install.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+start_time=`date +%s`
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
 ansible-playbook -vvv -i inventory.yml playbook.yml
+end_time=`date +%s`
+echo execution time was `expr $end_time - $start_time` s.
