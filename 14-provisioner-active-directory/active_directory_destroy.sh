229921e89ad202352a86e003e64625f0cdee1ac0
Anton Patsev <patsev.anton@gmail.com>
2022-03-12 14:17:51 +0600

    bash convert start time and end time to minutes

--- 14-provisioner-active-directory/active_directory_destroy.sh
+++ 14-provisioner-active-directory/active_directory_destroy.sh
@@ -3,6 +3,12 @@
 set -e
 
 start_time=`date +%s`
+date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform destroy -auto-approve
 end_time=`date +%s`
-echo execution time was `expr $end_time - $start_time` s.
+date2=$(date +"%s")
+echo "###############"
+echo Execution time was `expr $end_time - $start_time` s.
+DIFF=$(($date2-$date1))
+echo "Duration: $(($DIFF / 3600 )) hours $((($DIFF % 3600) / 60)) minutes $(($DIFF % 60)) seconds"
+echo "###############"
