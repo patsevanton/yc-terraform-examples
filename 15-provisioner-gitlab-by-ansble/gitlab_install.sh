b3c11bbcbb4ba439ad24eff209f481876d4e4c66
Anton Patsev <patsev.anton@gmail.com>
2022-03-13 12:15:48 +0600

    read -p "Enter GITLAB_ROOT_PASSWORD: " GITLAB_ROOT_PASSWORD

--- 15-provisioner-gitlab-by-ansble/gitlab_install.sh
+++ 15-provisioner-gitlab-by-ansble/gitlab_install.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+read -p "Enter GITLAB_ROOT_PASSWORD: " GITLAB_ROOT_PASSWORD
+
 start_time=`date +%s`
 date1=$(date +"%s")
 TF_IN_AUTOMATION=1 terraform init
 TF_IN_AUTOMATION=1 terraform apply -auto-approve
-ansible-playbook -vvv -i inventory.yml playbook.yml
+sed -e s/GITLAB_ROOT_PASSWORD_ENV/$GITLAB_ROOT_PASSWORD/ -i playbook.yml
+ansible-playbook -i inventory.yml playbook.yml
 end_time=`date +%s`
 date2=$(date +"%s")
 echo "###############"
@@ -14,3 +17,4 @@ echo Execution time was `expr $end_time - $start_time` s.
 DIFF=$(($date2-$date1))
 echo "Duration: $(($DIFF / 3600 )) hours $((($DIFF % 3600) / 60)) minutes $(($DIFF % 60)) seconds"
 echo "###############"
+echo "GITLAB_ROOT_PASSWORD is: $GITLAB_ROOT_PASSWORD"
