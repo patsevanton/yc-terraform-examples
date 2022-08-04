070926aad8d3382cebd8c99e9dfb33f8ef295f2f
Anton Patsev <patsev.anton@gmail.com>
2022-08-04 22:08:59 +0600

    copy 09-create-fixed-size-instance-group

--- 14-terraform-blue-green-deployment/main.tf
+++ 14-terraform-blue-green-deployment/main.tf
@@ -2,32 +2,61 @@ data "yandex_compute_image" "ubuntu-20-04" {
   family = "ubuntu-2004-lts"
 }
 
-resource "yandex_compute_instance" "app" {
-  name        = "app-${count.index}"
-  platform_id = "standard-v3"
-  count       = var.instances
+## Create SA sa-compute-admin
+resource "yandex_iam_service_account" "sa-compute-admin" {
+  folder_id = var.yc_folder_id
+  name      = "sa-compute-admin"
+}
 
-  resources {
-    cores  = 2
-    memory = 2
-  }
+## Grant permissions sa-compute-admin
+resource "yandex_resourcemanager_folder_iam_member" "sa-compute-admin-permissions" {
+  folder_id = var.yc_folder_id
+  role      = "compute.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-compute-admin.id}"
+}
 
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu-20-04.id
+resource "yandex_compute_instance_group" "ig-1" {
+  name               = "fixed-ig"
+  folder_id          = var.yc_folder_id
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  depends_on         = [yandex_resourcemanager_folder_iam_member.sa-compute-admin-permissions]
+  instance_template {
+    platform_id = "standard-v3"
+    resources {
+      cores  = 2
+      memory = 2
+    }
+
+    boot_disk {
+      initialize_params {
+        image_id = data.yandex_compute_image.ubuntu-20-04.id
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.network-1.id
+      subnet_ids = [yandex_vpc_subnet.subnet-1.id]
+    }
+
+    metadata = {
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
     }
   }
 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
-    nat       = true
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
   }
 
-  metadata = {
-    ssh-keys  = "ubuntu:${file(var.public_key_path)}"
-    user-data = file("cloud-init.yaml")
+  allocation_policy {
+    zones = ["ru-central1-c"]
   }
 
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
 }
 
 resource "yandex_vpc_network" "network-1" {
@@ -36,61 +65,7 @@ resource "yandex_vpc_network" "network-1" {
 
 resource "yandex_vpc_subnet" "subnet-1" {
   name           = "subnet1"
-  zone           = "ru-central1-b"
+  zone           = "ru-central1-c"
   network_id     = yandex_vpc_network.network-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
-}
-
-resource "yandex_lb_target_group" "app_group" {
-  name      = "app-target-group"
-  folder_id = var.yc_folder_id
-
-  dynamic "target" {
-    for_each = [for s in yandex_compute_instance.app : {
-      address   = s.network_interface.0.ip_address
-      subnet_id = s.network_interface.0.subnet_id
-    }]
-
-    content {
-      subnet_id = target.value.subnet_id
-      address   = target.value.address
-    }
-  }
-
-}
-
-resource "yandex_lb_network_load_balancer" "lb-app" {
-  name = "loadbalancer"
-  type = "external"
-
-  listener {
-    name        = "listener"
-    port        = 80
-    target_port = 80
-
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
-
-  attached_target_group {
-    target_group_id = yandex_lb_target_group.app_group.id
-    healthcheck {
-      name = "http"
-      http_options {
-        port = 80
-        path = "/"
-      }
-    }
-  }
-}
-
-output "loadbalancer_ip_address" {
-  value = yandex_lb_network_load_balancer.lb-app.listener.*.external_address_spec[0].*.address
-}
-
-# Output values
-output "public-ip-address-for" {
-  description = "Public IP address"
-  value       = yandex_compute_instance.app[*].network_interface.0.nat_ip_address
 }
