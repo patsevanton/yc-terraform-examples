73da12b4af0bfdde2fdf7448e43a661094e69d99
Anton Patsev <patsev.anton@gmail.com>
2022-08-13 10:11:03 +0600

    15-fixed-size-instance-group-lb

---
+++ 15-fixed-size-instance-group-lb/main.tf
@@ -0,0 +1,71 @@
+data "yandex_compute_image" "ubuntu-20-04" {
+  family = "ubuntu-2004-lts"
+}
+
+## Create SA sa-compute-admin
+resource "yandex_iam_service_account" "sa-compute-admin" {
+  folder_id = var.yc_folder_id
+  name      = "sa-compute-admin"
+}
+
+## Grant permissions sa-compute-admin
+resource "yandex_resourcemanager_folder_iam_member" "sa-compute-admin-permissions" {
+  folder_id = var.yc_folder_id
+  role      = "compute.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.sa-compute-admin.id}"
+}
+
+resource "yandex_compute_instance_group" "ig-1" {
+  name               = "fixed-ig"
+  folder_id          = var.yc_folder_id
+  service_account_id = yandex_iam_service_account.sa-compute-admin.id
+  depends_on         = [yandex_resourcemanager_folder_iam_member.sa-compute-admin-permissions]
+  instance_template {
+    platform_id = "standard-v3"
+    resources {
+      cores  = 2
+      memory = 2
+    }
+
+    boot_disk {
+      initialize_params {
+        image_id = data.yandex_compute_image.ubuntu-20-04.id
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.network-1.id
+      subnet_ids = [yandex_vpc_subnet.subnet-1.id]
+    }
+
+    metadata = {
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-c"]
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-c"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
---
+++ 15-fixed-size-instance-group-lb/readme.md
@@ -0,0 +1,46 @@
+### Get FOLDER_ID
+```
+FOLDER_ID=$(yc config get folder-id)
+```
+
+### Create service-account
+```
+yc iam service-account create --name computeadmin
+```
+
+### Get id of service-account
+```
+SA_ID=$(yc iam service-account get --name computeadmin --format json | jq .id -r)
+```
+
+### Assign a role to the computeadmin service account using its ID:
+```
+yc resource-manager folder add-access-binding --id $FOLDER_ID --role compute.admin --subject serviceAccount:$SA_ID
+```
+
+### Create a network
+```
+yc vpc network create --name custom-network
+```
+
+### Get network list
+```
+yc vpc network list
+```
+
+### Show availability zones
+```
+yc compute zone list
+```
+
+### Create an instance group in the default folder
+```
+yc compute instance-group create --file specification.yaml
+```
+
+Links:
+ - https://postgrespro.ru/products/postgrespro/yandexcloud/enterprise
+ - https://cloud.yandex.com/en/docs/compute/operations/instance-groups/create-fixed-group
+ - https://git.cloud-team.ru/lections/kubernetes_setup/blob/master/terraform/k8s-cluster.tf
+ - https://github.com/yandextf/terraform-yandex-actions
+ 
