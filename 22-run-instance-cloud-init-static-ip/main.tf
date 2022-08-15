3080a46c121ac92a7320d09620e378635d7e9828
Anton Patsev <patsev.anton@gmail.com>
2022-08-15 14:55:32 +0600

    22-run-instance-cloud-init-static-ip

---
+++ 22-run-instance-cloud-init-static-ip/main.tf
@@ -0,0 +1,54 @@
+data "yandex_compute_image" "ubuntu-20-04" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+
+  name        = "linux-vm"
+  platform_id = "standard-v3"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu-20-04.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys  = "ubuntu:${file(var.public_key_path)}"
+    user-data = file("cloud-init.yaml")
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_address" "addr" {
+  name = "static-ip"
+  external_ipv4_address {
+    zone_id = "ru-central1-b"
+  }
+}
+
+# Output values
+output "public-ip-address-for-vm-1" {
+  description = "Public IP address for vm-1"
+  value       = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
