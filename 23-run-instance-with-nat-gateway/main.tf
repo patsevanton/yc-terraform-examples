c80a75a4a390e34e18eccc002623c31f8b34bc17
Anton Patsev <patsev.anton@gmail.com>
2022-09-22 20:07:34 +0600

    24-yandex_vpc_route_table

--- 23-run-instance-with-nat-gateway/main.tf
+++ 23-run-instance-with-nat-gateway/main.tf
@@ -36,4 +36,20 @@ resource "yandex_vpc_subnet" "subnet1" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
+  route_table_id = yandex_vpc_route_table.rt.id
+}
+
+resource "yandex_vpc_gateway" "nat_gateway" {
+  name = "test-gateway"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "rt" {
+  name       = "test-route-table"
+  network_id = yandex_vpc_network.network1.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat_gateway.id
+  }
 }
