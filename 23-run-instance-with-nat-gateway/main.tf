d66cd2652b679f4709203ab785f0899b63bc8a02
Anton Patsev <patsev.anton@gmail.com>
2022-09-22 20:08:20 +0600

    remove vm from 23-run-instance-with-nat-gateway

--- 23-run-instance-with-nat-gateway/main.tf
+++ 23-run-instance-with-nat-gateway/main.tf
@@ -1,32 +1,3 @@
-data "yandex_compute_image" "ubuntu-20-04" {
-  family = "ubuntu-2004-lts"
-}
-
-resource "yandex_compute_instance" "vm-1" {
-
-  name        = "linux-vm"
-  platform_id = "standard-v3"
-
-  resources {
-    cores  = 2
-    memory = 2
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu-20-04.id
-    }
-  }
-
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet1.id
-  }
-
-  metadata = {
-    ssh-keys = "yc-user:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
-
 resource "yandex_vpc_network" "network1" {
   name = "network1"
 }
